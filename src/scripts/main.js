fb0ed825e5c86bc9dd3addf203d0e7dc7c516dc3
= <=>
2023-04-01 19:58:26 +0200

    feat: initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,119 @@
+import villes from './villes.js';
+window.addEventListener('load', initApp);
+
+//Informations du courreur
+var athlete = {
+    firstname: '',
+    lastname: '',
+    profile_medium: '',
+    state: '',
+    city: '',
+    distanceTotale: 0
+}
+
+/**
+ * Init the app
+ */
+function initApp() {
+  const nom = document.getElementById('athlete-name');
+  const city = document.getElementById('athlete-city');
+  const distance = document.getElementById('athlete-distance');
+  const profile = document.getElementById('athlete-profile');
+  const athleteProgress = document.getElementById('athlete-progress');
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  if (urlParams.get('code')) {
+    connexionApp(urlParams.get('code'), nom, city, distance, profile, athleteProgress);
+  }
+}
+
+/**
+ * Get the token from Strava
+ * @param {string} code
+ */
+function connexionApp(code, nom, city, distance, profile, athleteProgress) {
+    fetch('https://www.strava.com/oauth/token', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            code,
+            client_id: '104903',
+            client_secret: 'd18a391eca021190475ff6ab018c1686ed0514b3',
+            grant_type: 'authorization_code'
+        })
+    })
+    .then(response => {
+        response.json().then((data) => {
+          console.log(data);
+          athlete.firstname = data.athlete.firstname
+          athlete.lastname = data.athlete.lastname
+          athlete.profile_medium = data.athlete.profile_medium
+          athlete.state = data.athlete.state
+          athlete.city = data.athlete.city
+
+          getInformation(data.athlete.id, data.access_token, nom, city, distance, profile, athleteProgress);
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+/**
+ * Get information about the user
+ * @param {number} id
+ * @param {string} token
+ */
+function getInformation(id, token, nom, city, distance, profile, athleteProgress) {
+  fetch(`https://www.strava.com/api/v3/athletes/${id}/stats?access_token=${token}`, {
+    method: 'get',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    }
+  })
+  .then(response => {
+      response.json().then((data) => {
+          console.log(data)
+          athlete.distanceTotale = data.all_run_totals.distance / 1000
+      }).then(() => {
+        setInformation(nom, city, distance, profile, athleteProgress)
+        document.querySelector('.btn').style.display = 'none';
+      })
+  })
+  .catch(error => {
+      console.log(error)
+  })
+}
+
+/**
+ * Set the information in the HTML
+ * @param {HTMLElement} name
+ * @param {HTMLElement} city
+ * @param {HTMLElement} distance
+ * @param {HTMLElement} profile
+ */
+function setInformation(name, city, distance, profile, athleteProgress) {
+    name.innerHTML = `${athlete.firstname} ${athlete.lastname}`
+    city.innerHTML = `${athlete.city}, ${athlete.state}`
+    distance.innerHTML = `Distance totale parcouru : ${athlete.distanceTotale} km`
+    profile.src = athlete.profile_medium
+    for (let i = 0; i < athlete.distanceTotale; i++) {
+      athleteProgress.value = i;
+    }
+    let find = false;
+    villes.villes.forEach((ville, index) => {
+      if (athlete.distanceTotale < ville.supperficie && !find) {
+        find = true
+        athleteProgress.max = ville.supperficie
+        athleteProgress.min = index != 0 ? ville.supperficie[index -1] : ville.supperficie
+        document.getElementById('villeDepart').innerHTML = index != 0 ? villes.villes[index -1].nom : villes.villes[index].nom
+        document.getElementById('villeArrivee').innerHTML = ville.nom
+        document.getElementById('valueDepart').innerHTML = index != 0 ? `${villes.villes[index -1].supperficie}km` : `${villes.villes[index].supperficie}km`
+        document.getElementById('valueArrivee').innerHTML = `${athleteProgress.max}km`
+      }
+    });
+}
---
+++ src/scripts/villes.js
@@ -0,0 +1,79 @@
+export default {
+    villes: [
+        {
+            nom: "Manille",
+            pays: "Philippines",
+            supperficie: 42.88,
+        },
+        {
+            nom: "Osaka",
+            pays: "Japon",
+            supperficie: 222.30,
+        },
+        {
+            nom: "Dhaka",
+            pays: "Bangladesh",
+            supperficie: 306.4,
+        },
+        {
+            nom: "Mumbai",
+            pays: "Inde",
+            supperficie: 603.4,
+        },
+        {
+            nom: "Séoul",
+            pays: "Corée du Sud",
+            supperficie: 605.2,
+        },
+        {
+            nom: "Jakarta",
+            pays: "Indonésie",
+            supperficie: 664.0,
+        },
+        {
+            nom: "New York",
+            pays: "États-Unis",
+            supperficie: 1214.4,
+        },
+        {
+            nom: "Delhi",
+            pays: "Inde",
+            supperficie: 1481.0,
+        },
+        {
+            nom: "Mexico",
+            pays: "Mexique",
+            supperficie: 1485.0,
+        },
+        {
+            nom: "São Paulo",
+            pays: "Brésil",
+            supperficie: 1521.0,
+        },
+        {
+            nom: "Tokyo",
+            pays: "Japon",
+            supperficie: 2190.93,
+        },
+        {
+            nom: "Le Caire",
+            pays: "Égypte",
+            supperficie: 3085.0,
+        },
+        {
+            nom: "Shanghai",
+            pays: "Chine",
+            supperficie: 6340.0,
+        },
+        {
+            nom: "Canton ou Guangzhou",
+            pays: "Chine",
+            supperficie: 7434.0,
+        },
+        {
+            nom: "Pékin",
+            pays: "Chine",
+            supperficie: 16411.0,
+        }
+    ]
+}
